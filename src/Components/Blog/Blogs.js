39d39c157d7da676e8d2e44d3568a1fa9653d8d9
SabbirSami <smd71430@gmail.com>
2022-07-08 23:50:24 +0600

    blogs section header image set done

--- src/Components/Blog/Blogs.js
+++ src/Components/Blog/Blogs.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Blogs = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div className="container-fluid blog-bg">
+                <div className="py-5">
+                    <h1 className="py-5 display-2 text-center text-white">
+                        #Blogs
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Blogs;
