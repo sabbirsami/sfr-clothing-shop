55695531f7e8e6e6e25830f03ed78f2d717ce58d
SabbirSami <smd71430@gmail.com>
2022-07-08 23:38:40 +0600

    shop section done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Product from "../Home/Product";
+import { HiOutlineMinus } from "react-icons/hi";
 
 const Shop = () => {
     const { data: products, isLoading } = useQuery("products", () =>
@@ -14,11 +15,23 @@ const Shop = () => {
             <div className="container">
                 <div className="py-3">
                     <div className="row">
+                        <div className="col-lg-3">
+                            <div className="d-flex pt-5 align-items-center">
+                                <div className="py-3 text-start">
+                                    <h1 className="display-1 heading-font">
+                                        Shop
+                                    </h1>
+                                    <p className="fs-5 fw-light">
+                                        Summer Collection New Modern Design{" "}
+                                        <HiOutlineMinus />
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                         {products.map((product, index) => (
                             <Product key={index} product={product} />
                         ))}
                     </div>
-                    <p className="text-center fs-5 fw-light pt-4">Load More</p>
                 </div>
             </div>
         </div>
