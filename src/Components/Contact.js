812f56ca550e022f0bac7b4e0ba83ed6873c0592
SabbirSami <smd71430@gmail.com>
2022-07-09 11:08:20 +0600

    contact page header done

---
+++ src/Components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div>
+            <div className="container-fluid contact-bg">
+                <div className="py-5">
+                    <h1 className="py-5 display-2 text-center text-white">
+                        #About us
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
