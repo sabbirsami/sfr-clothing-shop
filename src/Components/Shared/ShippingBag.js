b64f8772ec110ab41f2f219442be85bfa2be134a
SabbirSami <smd71430@gmail.com>
2022-07-10 13:26:41 +0600

    add cancle option fetch

--- src/Components/Shared/ShippingBag.js
+++ src/Components/Shared/ShippingBag.js
@@ -4,32 +4,47 @@ import { Link } from "react-router-dom";
 import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
 
 const ShippingBag = () => {
-    const { data: orders, isLoading } = useQuery("orders", () =>
+    const {
+        data: orders,
+        isLoading,
+        refetch,
+    } = useQuery("orders", () =>
         fetch("http://localhost:5000/orders").then((res) => res.json())
     );
     if (isLoading) {
         return <p>Loading</p>;
     }
 
+    const handleCancel = (id) => {
+        console.log(id);
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: "DELETE",
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                alert("Order cancel");
+                refetch();
+            });
+    };
     return (
         <div>
             <div className="container">
                 <div className="row justify-content-evenly">
                     <div className="col-lg-9">
-                        <div className="py-5 px-2">
-                            <div className="d-flex align-items-center justify-content-between pb-4">
+                        <div className="py-5 px-3">
+                            <div className="d-flex align-items-center border-bottom justify-content-between pb-4">
                                 <h2>Shopping Cart</h2>
                                 <h2>2 Items</h2>
                             </div>
                             <div className="">
                                 <div
-                                    className="row align-items-center justify-content-between rounded-2 mb-3 py-4"
-                                    style={{ backgroundColor: "#F5F6FA" }}
+                                    className="row align-items-center justify-content-between rounded-2 py-4"
+                                    // style={{ backgroundColor: "#F5F6FA" }}
                                 >
                                     <div className="col-4 fw-semibold">
                                         PRODUCT DETAILS
                                     </div>
-                                    <div className="col-2 fw-semibold">
+                                    <div className="col-1 fw-semibold">
                                         QUANTITY
                                     </div>
                                     <div className="col-1 fw-semibold">
@@ -38,7 +53,7 @@ const ShippingBag = () => {
                                     <div className="col-1 fw-semibold">
                                         TOTAL
                                     </div>
-                                    <div className="col-1 fw-semibold">
+                                    <div className="col-2 fw-semibold">
                                         REMOVE
                                     </div>
                                 </div>
@@ -61,7 +76,7 @@ const ShippingBag = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="col-2">
+                                            <div className="col-1">
                                                 {order.quantity}
                                             </div>
                                             <div className="col-1">
@@ -70,8 +85,13 @@ const ShippingBag = () => {
                                             <div className="col-1">
                                                 ${order.price * order.quantity}
                                             </div>
-                                            <div className="col-1 text-start">
-                                                <button className="btn ps-0 border-0 text-danger">
+                                            <div className="col-2 text-start">
+                                                <button
+                                                    onClick={() =>
+                                                        handleCancel(order._id)
+                                                    }
+                                                    className="btn ps-0 border-0 text-danger"
+                                                >
                                                     Remove
                                                 </button>
                                             </div>
