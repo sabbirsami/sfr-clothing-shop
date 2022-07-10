6e65e8f366f9bea40b755d2ce0188538c3b08927
SabbirSami <smd71430@gmail.com>
2022-07-10 16:52:57 +0600

    add Loading component

---
+++ src/Components/Shared/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div
+            className="d-flex align-items-center justify-content-center"
+            style={{ height: "100vh" }}
+        >
+            <p>Loading...</p>
+        </div>
+    );
+};
+
+export default Loading;
