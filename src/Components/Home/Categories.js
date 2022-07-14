43d43dc3f5cb6c3e37ee892679769e461f312eed
SabbirSami <smd71430@gmail.com>
2022-07-15 00:02:32 +0600

    Categories component done

--- src/Components/Home/Categories.js
+++ src/Components/Home/Categories.js
@@ -1,9 +1,55 @@
 import React from "react";
+import cloth from "../../Images/categories/f9.png";
+import shoe from "../../Images/categories/shoes.png";
+import bag from "../../Images/categories/bag.png";
 
 const Categories = () => {
     return (
-        <div>
-            <h1>Categories</h1>
+        <div className="pt-4">
+            <div className="container py-4">
+                <h1 className="display-6 pb-3">Categories</h1>
+                <div className="row">
+                    <div
+                        className="col-lg-4  mt-2 border-end bg-gradient border-light border-3 "
+                        style={{ backgroundColor: "#EFEFEF" }}
+                    >
+                        <div className="d-flex justify-content-around align-items-center pb-2 ps-3">
+                            <div className="">
+                                <h2 className="m-0">Cloth</h2>
+                            </div>
+                            <div className="w-25">
+                                <img className="w-100" src={cloth} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="col-lg-4  mt-2 border-end bg-gradient border-light border-3 "
+                        style={{ backgroundColor: "#EFEFEF" }}
+                    >
+                        <div className="d-flex justify-content-around align-items-center pb-2 ps-3">
+                            <div className="">
+                                <h2 className="m-0">Cloth</h2>
+                            </div>
+                            <div className="w-25">
+                                <img className="w-100 p-2" src={bag} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="col-lg-4  mt-2 border-end bg-gradient border-light border-3 "
+                        style={{ backgroundColor: "#EFEFEF" }}
+                    >
+                        <div className="d-flex justify-content-around align-items-center pb-2 ps-3">
+                            <div className="">
+                                <h2 className="m-0">Cloth</h2>
+                            </div>
+                            <div className="w-25">
+                                <img className="w-100" src={shoe} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
