7e56fb75d1e45b15113492dde36d3bc4be55daa2
SabbirSami <smd71430@gmail.com>
2022-07-26 22:22:54 +0600

    fix subHeader for mobile

--- src/Components/Shared/SubHeader.js
+++ src/Components/Shared/SubHeader.js
@@ -6,12 +6,12 @@ const SubHeader = () => {
             <div className="container ">
                 <div className="row justify-content-between align-item-center py-2">
                     <div className="col-lg-7">
-                        <p className="m-0">
+                        <p className="m-0 text-lg-start text-center">
                             Telephone Enquiry +880 1970 706 676
                         </p>
                     </div>
                     <div className="col-lg-5 me-auto">
-                        <p className="text-end m-0">
+                        <p className="text-lg-end text-center m-0">
                             Currency : $USD | Language : English
                         </p>
                     </div>
