0c277800c828a89b84b9276615703dfe4e81cbd3
SabbirSami <smd71430@gmail.com>
2022-07-28 10:33:53 +0600

    make image into link

--- src/Components/Home/Product.js
+++ src/Components/Home/Product.js
@@ -11,35 +11,41 @@ const Products = ({ product }) => {
     // };
     return (
         <div className="col-lg-3 col-6 py-3">
-            <div className="p-1 rounded-2 border">
-                <div className="p-2">
-                    <img className="img-fluid rounded-2" src={image} alt="" />
+            <Link className="nav-link" to={`/shop/${_id}`}>
+                <div className="p-1 rounded-2 border">
+                    <div className="p-2">
+                        <img
+                            className="img-fluid rounded-2"
+                            src={image}
+                            alt=""
+                        />
+                    </div>
+                    <div className="p-2">
+                        <p className="mb-1 d-flex align-items-center justify-content-between">
+                            <small>{brand}</small>
+                            <small className="pe-1 text-warning">
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                            </small>
+                        </p>
+                        <h5>{name}</h5>
+                        <h3 className=" d-flex justify-content-between align-items-center">
+                            <p className="m-0">${price}</p>
+                            <Link
+                                title="Add to cart"
+                                to={`/shop/${_id}`}
+                                // onClick={() => handleAddToCart(_id)}
+                                className=" btn rounded-pill bg-alert-success"
+                            >
+                                <BsCart3 className="mb-1 mx-1 fw-semibold fs-5" />
+                            </Link>
+                        </h3>
+                    </div>
                 </div>
-                <div className="p-2">
-                    <p className="mb-1 d-flex align-items-center justify-content-between">
-                        <small>{brand}</small>
-                        <small className="pe-1 text-warning">
-                            <FaStar />
-                            <FaStar />
-                            <FaStar />
-                            <FaStar />
-                            <FaStar />
-                        </small>
-                    </p>
-                    <h5>{name}</h5>
-                    <h3 className=" d-flex justify-content-between align-items-center">
-                        <p className="m-0">${price}</p>
-                        <Link
-                            title="Add to cart"
-                            to={`/shop/${_id}`}
-                            // onClick={() => handleAddToCart(_id)}
-                            className=" btn rounded-pill bg-alert-success"
-                        >
-                            <BsCart3 className="mb-1 mx-1 fw-semibold fs-5" />
-                        </Link>
-                    </h3>
-                </div>
-            </div>
+            </Link>
         </div>
     );
 };
