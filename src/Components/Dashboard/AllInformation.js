76eaeb0bd9e3cbd6baf840e9011aab6a7ed093e3
SabbirSami <smd71430@gmail.com>
2022-07-29 22:24:55 +0600

    show total order number

--- src/Components/Dashboard/AllInformation.js
+++ src/Components/Dashboard/AllInformation.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { BsMinecartLoaded } from "react-icons/bs";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const AllInformation = () => {
     const [pageCount, setPageCount] = useState(0);
+    const { data: orderCount, isLoading } = useQuery("countOrder", () =>
+        fetch(`http://localhost:5000/ordersCount`, {
+            method: "GET",
+        }).then((res) => res.json())
+    );
 
     useEffect(() => {
         fetch("http://localhost:5000/productCount")
@@ -13,6 +19,11 @@ const AllInformation = () => {
                 setPageCount(counts);
             });
     }, []);
+    console.log(orderCount);
+
+    if (isLoading) {
+        return <Loading />;
+    }
     return (
         <div>
             <div className="container-fluid">
@@ -20,7 +31,9 @@ const AllInformation = () => {
                     <div className="col-lg-4">
                         <div className="p-4 bg-white m-2 rounded-3">
                             <h3 className="pb-2 fw-light">Total Order</h3>
-                            <h1 className="display-1 fw-semibold">32 </h1>
+                            <h1 className="display-1 fw-semibold">
+                                {orderCount?.count}
+                            </h1>
                         </div>
                     </div>
                     <div className="col-lg-4">
